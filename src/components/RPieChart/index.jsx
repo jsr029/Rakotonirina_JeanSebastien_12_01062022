d3a3f1f11508c48269b4520231c7647102deb60c
jsr029 <jsr059@gmail.com>
2022-06-15 13:29:09 +0200

    Update Pie

--- src/components/RPieChart/index.jsx
+++ src/components/RPieChart/index.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
-import { PieChart, Pie, ResponsiveContainer} from 'recharts'
+import { PieChart, Pie, ResponsiveContainer, Cell} from 'recharts'
 
 const RPieChart = ({ scoreUser }) => {        
         const score = scoreUser.score * 100
         // Sample data
         const data = [
         {name: '% de votre objectif', score: score},
-        {name: '100% de votre objectif', score: 100}
+        {name: '100% de votre objectif', score: 75-score}
         ]
-        
+        const COLORS = [
+                '#FF0000',
+                '#FFF'
+        ]
         
         return (
                 <>
                 <h2 className='sportSee-pie'>{score}% <br /> de votre <br /> objectif</h2>
                 <ResponsiveContainer width="100%" height={300}>
                 <PieChart width={340} height={340}>
-                <Pie data={data} dataKey="score" outerRadius={100} innerRadius={90} fill="#FF0000" />
+                        <Pie 
+                        data={data} 
+                        dataKey="score" 
+                        outerRadius={100} 
+                        innerRadius={90} 
+                        startAngle={90}
+                         >
+{
+          	data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>)
+          }
+                        </Pie>
                 </PieChart>
                 </ResponsiveContainer>
                 </>
